a43344c6845ed9395a6d6f97796786c9c5c9d665
hackerman70000 <bartek.dmitruk@gmail.com>
2024-02-29 20:46:57 +0100

    Data visualization.

--- data_visualization.py
+++ data_visualization.py
@@ -24,27 +24,37 @@ def print_info(df):
     print('Missing values: \n', df.isnull().sum(), '\n')
 
 
-def detect_outliers(df):
-    for col_name, col_values in df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']].items():
-        q1 = col_values.quantile(0.25)
-        q3 = col_values.quantile(0.75)
-        irq = q3 - q1
-        outliers = col_values[(col_values <= q1 - 1.5 * irq) | (col_values >= q3 + 1.5 * irq)]
-        percentage_outliers = len(outliers) * 100.0 / len(df)
-        print("Column {} outliers = {}%".format(col_name, round(percentage_outliers, 2)))
-
-
-def correlation_matrix(df):
+def plot_correlation_matrix(df):
     plt.figure(figsize=(18, 8))
-    sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f', linewidths=2)
-    plt.title('Correlation Matrix')
+    sns.heatmap(df.corr(), annot=True, cmap='crest', fmt='.2f', linewidths=2)
+    plt.title('Correlation Matrix', fontsize=20)
     save_image(plt, 'correlation_matrix.png')
     plt.show()
 
 
+def plot_distribution(df):
+    columns = df.columns[:-1]
+    plt.figure(figsize=(20, 10))
+    plt.suptitle('Density distribution of features', fontsize=20)
+
+    for i, column in enumerate(columns, start=1):
+        plt.subplot(3, 5, i)
+        sns.histplot(x=column, hue='class', data=df, palette='crest', kde=True)
+
+    save_image(plt, 'distribution')
+
+
+def plot_pairplot(df):
+    pairplot = sns.pairplot(df, hue='class', palette='viridis')
+    save_image(pairplot, 'pairplot.png')
+
+
 def plot(df):
-    correlation_matrix(df)
+    plot_correlation_matrix(df)
+    plot_distribution(df)
     plt.show()
+    plot_pairplot(df)
+    plt.tight_layout()
 
 
 def main():
@@ -61,9 +71,6 @@ def main():
     print_info(df)
     df = df.dropna()
     plot(df)
-
-
-# detect_outliers(df)
 
 
 if __name__ == '__main__':
